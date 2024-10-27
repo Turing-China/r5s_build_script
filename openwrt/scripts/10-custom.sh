70d1ce8fc6ff4f1420f5be93984595f18b4efbd4
Turing-China <107291340+Turing-China@users.noreply.github.com>
2024-10-27 13:05:25 +0800

    Update 10-custom.sh

--- openwrt/scripts/10-custom.sh
+++ openwrt/scripts/10-custom.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
 # 自定义脚本
+# ImmortalWrt
+# 获取Syncthing插件
 
+# 输出当前路径
+echo "当前路径: $(pwd)"
+
+git clone -b openwrt-23.05 https://github.com/coolsnowwolf/luci temp_luci && \
+mkdir -p package/luci-app-syncthing && \
+cp -r temp_luci/applications/luci-app-syncthing/*  package/luci-app-syncthing/ && \
+rm -rf temp_luci
+
+# 输出当前路径
+echo "当前路径: $(pwd)"
+
+# 检查目录是否存在
+if [ -d "$TARGET_DIR" ]; then
+    echo "目录 $TARGET_DIR 下的所有子目录和文件："
+    find "$TARGET_DIR"
+else
+    echo "目录 $TARGET_DIR 不存在。"
+fi
