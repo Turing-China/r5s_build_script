8921effa3a271ceed25f151e1c8e5b540f4776cc
Turing-China <107291340+Turing-China@users.noreply.github.com>
2024-10-27 12:39:37 +0800

    Update build.sh

--- openwrt/build.sh
+++ openwrt/build.sh
@@ -236,12 +236,27 @@ EOF
 # 自定义脚本
 # ImmortalWrt
 # 获取Syncthing插件
+
+# 输出当前路径
+echo "当前路径: $(pwd)"
 git clone -b openwrt-23.05 https://github.com/immortalwrt/luci temp_luci && \
 mkdir -p package/luci-app-syncthing && \
 cp -r temp_luci/applications/luci-app-syncthing/*  package/luci-app-syncthing/ && \
 rm -rf temp_luci
+
 # 输出当前路径
 echo "当前路径: $(pwd)"
+
+# 指定要列出文件的目录
+TARGET_DIR="package/luci-app-syncthing"
+
+# 检查目录是否存在
+if [ -d "$TARGET_DIR" ]; then
+    echo "目录 $TARGET_DIR 下的文件："
+    ls "$TARGET_DIR"
+else
+    echo "目录 $TARGET_DIR 不存在。"
+fi
 
 # Init feeds
 [ "$(whoami)" = "runner" ] && group "feeds update -a"
