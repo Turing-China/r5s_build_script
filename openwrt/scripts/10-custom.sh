da196194cc23b7c1da927f9d7ace267d3c2e46ce
Turing-China <107291340+Turing-China@users.noreply.github.com>
2024-10-27 10:50:19 +0800

    Update 10-custom.sh

--- openwrt/scripts/10-custom.sh
+++ openwrt/scripts/10-custom.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 # 自定义脚本
+# 获取Syncthing插件
+git clone -b openwrt-23.05 https://github.com/immortalwrt/luci temp_luci && \
+cp -r temp_luci/applications/luci-app-syncthing/* package/luci-app-syncthing/ && \
+rm -rf temp_luci
